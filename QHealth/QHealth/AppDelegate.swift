5ed708cd12c4bd60dcb1e98cdcc2ff3ddac36029
Anand <Anand@macproqrit0004.local>
2017-03-03 16:09:11 +0300

    project modified

--- QHealth/QHealth/AppDelegate.swift
+++ QHealth/QHealth/AppDelegate.swift
@@ -13,12 +13,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    var tabBar: UITabBarController?
+    var mainController:MainViewController!
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame:UIScreen.main.bounds)
+        createTabController()
+        self.window?.makeKeyAndVisible()
+
         return true
     }
 
+    func createTabController() {
+        mainController = MainViewController(nibName:"MainViewController",bundle:nil)
+        self.tabBar = UITabBarController()
+        self.tabBar?.setViewControllers([mainController], animated: true)
+        self.window?.rootViewController = tabBar
+        
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
